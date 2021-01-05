96b30759b614cac1bd2fdccc15253d87f6ef3a84
cowdingus <zaki.nadhif45@gmail.com>
2021-01-05 21:35:18 +0700

    Cleaned main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,30 @@
 #include "Relay.hpp"
 #include "TimedAction.hpp"
+#include "Tasks/NormalTask.hpp"
 
 #include <Arduino.h>
 
-void processInput();
-void doNormal();
-void doIdle();
 void updateDeltaTime();
 
 Relay relay1(2);
 Relay relay2(4);
 
-enum class Task
-{
-  Idle,
-  Normal
-};
-
-Task currentTask = Task::Normal;
-
 unsigned long previousTime = 0;
 unsigned long time = 0;
 unsigned long deltaTime = time - previousTime;
+
+NormalTask normalTask(relay1, relay2);
 
 void setup() 
 {
   Serial.begin(9600);
 }
 
-void executeCurrentTask()
-{
-  switch (currentTask)
-  {
-    case Task::Idle:
-      doIdle();
-      break;
-    case Task::Normal:
-      doNormal();
-      break;
-    default:
-      doIdle();
-      break;
-  }
-}
-
 void loop()
 {
   updateDeltaTime();
-  processInput();
-  executeCurrentTask();
-}
 
-void processInput()
-{
-  String input = Serial.readStringUntil(';');
-  Serial.flush();
-  if (input.equalsIgnoreCase("Idle"))
-    currentTask = Task::Idle;
-  else if (input.equalsIgnoreCase("Normal"))
-    currentTask = Task::Normal;
-  else if (input.equalsIgnoreCase("getRelay(1).state"))
-    Serial.println(stringify(relay1));
-  else if (input.equalsIgnoreCase("getRelay(2).state"))
-    Serial.println(stringify(relay2));
-}
-
-void doNormal()
-{
-  static TimedAction setUpDownR1(
-    1000, 
-    []() 
-    {
-      if (relay1.getState() == Relay::State::Down)
-        relay1.setUp(); 
-      else
-      {
-        relay1.setDown();
-      }
-    }
-  );
-  
-  static TimedAction setUpDownR2(
-    1000,
-    []()
-    {
-      if (relay2.getState() == Relay::State::Down)
-        relay2.setUp();
-      else
-      {
-        relay2.setDown();
-      }
-    }
-  );
-
-  setUpDownR1.update(deltaTime);
-  setUpDownR2.update(deltaTime);
-}
-
-void doIdle()
-{
-  delay(100);
+  normalTask.run(deltaTime);
 }
 
 void updateDeltaTime()
