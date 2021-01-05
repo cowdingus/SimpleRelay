b569d250f135c6988c6fed91a3614ace90f6008e
cowdingus <zaki.nadhif45@gmail.com>
2021-01-05 21:35:11 +0700

    Completed NormalTask

--- include/Tasks/NormalTask.hpp
+++ include/Tasks/NormalTask.hpp
@@ -2,15 +2,35 @@
 
 #include "Task.hpp"
 #include "TimedAction.hpp"
+#include "Callback.hpp"
+
+class Relay;
 
 class NormalTask : public Task
 {
 public:
-  NormalTask();
+  NormalTask(Relay& relay1, Relay& relay2);
 
   void run(unsigned long deltaTime);
 
 private:
+  class CToggleRelay : public Callback
+  {
+  public:
+    CToggleRelay(Relay& relay);
+
+    void invoke() override;
+
+  private:
+    Relay& relay;
+  };
+
   TimedAction relay1Action;
   TimedAction relay2Action;
+
+  Relay& relay1;
+  Relay& relay2;
+
+  CToggleRelay actionCallback1;
+  CToggleRelay actionCallback2;
 };
--- src/Tasks/NormalTask.cpp
+++ src/Tasks/NormalTask.cpp
@@ -1,13 +1,42 @@
 #include "Tasks/NormalTask.hpp"
+#include "Relay.hpp"
 
-NormalTask::NormalTask()
-  : 
-  Task("Normal")
+#include <Arduino.h>
+
+NormalTask::NormalTask(Relay& relay1, Relay& relay2)
+  :
+  Task("Normal"),
+  relay1(relay1),
+  relay2(relay2),
+  actionCallback1(relay1),
+  actionCallback2(relay2)
 {
+  relay1Action.setIntervalDuration(1000);
+  relay1Action.attachActionCallback(&actionCallback1);
 
+  relay2Action.setIntervalDuration(1000);
+  relay2Action.attachActionCallback(&actionCallback2);
+
+  relay1Action.setActive(true);
+  relay2Action.setActive(true);
 }
 
 void NormalTask::run(unsigned long deltaTime)
 {
-  
+  relay1Action.update(deltaTime);
+  relay2Action.update(deltaTime);
+}
+
+NormalTask::CToggleRelay::CToggleRelay(Relay& relay)
+  : relay(relay)
+{
+
+}
+
+void NormalTask::CToggleRelay::invoke()
+{
+  if (relay.getState() == Relay::State::Down)
+    relay.setUp();
+  else
+    relay.setDown();
 }
