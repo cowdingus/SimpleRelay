e4437451f88926868d90b49fc0f625b98b128e59
cowdingus <zaki.nadhif45@gmail.com>
2021-01-06 10:25:54 +0700

    Make the code have a fixed array interface, instead of "semi-fixed"

--- include/AlternatingTimedActions.hpp
+++ include/AlternatingTimedActions.hpp
@@ -8,29 +8,18 @@ class AlternatingTimedActions
 public:
   AlternatingTimedActions();
 
-  const TimedAction* getAction(unsigned int actionId) const;
-  unsigned int addAction(TimedAction* timedAction);
-  void removeAction(unsigned int actionId);
-
   void update(unsigned long deltaTime);
 
   unsigned int getTurn() const;
   void setTurn(unsigned int turn);
 
-  unsigned int getMaxSize() const;
   unsigned int getSize() const;
 
+  TimedAction*& at(unsigned int index);
+  TimedAction*& operator[](unsigned int index);
+
 private:
-  struct ActionData
-  {
-    const unsigned int id = 0;
-    TimedAction* timedAction;
-  };
-
-  ActionData* actionData[size] = {nullptr};
-
-  TimedAction* getTimedActionFromId(unsigned int id);
+  ActionDatum* actionData[size] = {nullptr};
 
   unsigned int turn = 0;
-  unsigned int occupiedSize = 0;
 };
