171b3b7a9df2f255b4c3f9099fc39a36351ef653
cowdingus <zaki.nadhif45@gmail.com>
2021-01-09 16:43:30 +0700

    Added DelayedActions.

---
+++ src/Tasks/DATestTask.cpp
@@ -0,0 +1,50 @@
+#include "Tasks/DATestTask.hpp"
+
+#include "Relay.hpp"
+
+#include <Arduino.h>
+
+DATestTask::DATestTask(Relay& relay, uint16_t ledPin)
+  :
+  Task("DATest"),
+  cToggleRelay(relay),
+  cToggleLed(ledPin, 1)
+{
+  delayedActions.setAction(&cToggleLed, 0, 0);
+  delayedActions.setAction(&cToggleLed, 200, 1);
+  delayedActions.setAction(&cToggleRelay, 0, 2);
+  delayedActions.setAction(&cToggleRelay, 200, 3);
+
+  delayedActions.setTurn(0);
+}
+
+void DATestTask::run(uint32_t deltaTime)
+{
+  delayedActions.update(deltaTime); 
+}
+
+DATestTask::CToggleRelay::CToggleRelay(Relay& relay)
+  : relay(relay)
+{
+
+}
+
+void DATestTask::CToggleRelay::invoke()
+{
+  if (relay.getState() == Relay::State::Down)
+    relay.setUp();
+  else
+    relay.setDown();
+}
+
+DATestTask::CToggleLed::CToggleLed(uint16_t ledPin, uint8_t lastState)
+  : ledPin(ledPin), ledState(lastState)
+{
+
+}
+
+void DATestTask::CToggleLed::invoke()
+{
+  ledState = !ledState;
+  digitalWrite(ledPin, ledState);
+}
