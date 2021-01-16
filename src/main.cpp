802bb306640951d32d94f9345141a6a852e53fea
cowdingus <zaki.nadhif45@gmail.com>
2021-01-16 21:52:28 +0700

    Cleard main.cpp from unnecessary stuff

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,12 @@
 
 #include "RepeatingAction.hpp"
 
-#include "Tasks/IdleTask.hpp"
-#include "Tasks/DATestTask.hpp"
-
-#include <Wire.h>
 #include <RTClib.h>
-
 #include <Arduino.h>
+
+#ifdef DEBUG
+  #include <avr8-stub.h>
+#endif
 
 #if SERIAL_ALLOWED
 #include <assert.h>
@@ -29,20 +28,9 @@ void __assert(const char *__func, const char *__file, int __lineno, const char *
 }
 #endif
 
-#ifdef DEBUG
-  #include <avr8-stub.h>
-#endif
-
-IdleTask idleTask;
-DATestTask daTestTask(4, 6);
-
-String currentTask = "DATest";
-
 uint32_t previousTime = 0;
 uint32_t timeNow = 0;
 uint32_t deltaTime = timeNow - previousTime;
-
-RTC_DS3231 rtc;
 
 void setup() 
 {
@@ -53,55 +41,6 @@ void setup()
   #ifdef DEBUG
   debug_init();
   #endif
-
-  Wire.begin();
-  
-  if (!rtc.begin())
-  {
-  #if SERIAL_ALLOWED
-    Serial.println("Failed to begin connection with RTC_DS3231");
-  #endif
-  }
-
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-
-  pinMode(6, OUTPUT);
-}
-
-void processInput()
-{
-  #if SERIAL_ALLOWED
-  String input = Serial.readStringUntil('(');
-
-  if (input.equals("setTask"))
-  {
-    String taskName = Serial.readStringUntil(')');
-
-    if (taskName.equalsIgnoreCase(idleTask.getName()))
-      currentTask = "Idle";
-    else if (taskName.equalsIgnoreCase(daTestTask.getName()))
-      currentTask = "DATest";
-    else
-      Serial.println("No such registered task \'" + taskName + "\'");
-  }
-  if (input.equals("getTime"))
-  {
-    String param = Serial.readStringUntil(')');
-
-    DateTime now = rtc.now();
-    Serial.println(now.toString("YYYY/MM/DD"));
-  }
-  #endif
-}
-
-void executeCurrentTask()
-{
-  if (currentTask.equals("Idle"))
-    idleTask.run();
-  else if (currentTask.equals("DATest"))
-    daTestTask.run(deltaTime);
-  else
-    idleTask.run();
 }
 
 void updateDeltaTime()
@@ -114,6 +53,4 @@ void updateDeltaTime()
 void loop()
 {
   updateDeltaTime();
-  processInput();
-  executeCurrentTask();
 }
