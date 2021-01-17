71f0d1c8930f6bc730afd24fa5f53c9c15df08e1
cowdingus <zaki.nadhif45@gmail.com>
2021-01-17 20:27:56 +0700

    Added some TimeMatchedAction tests

--- test/test_embedded_TimeMatchedAction/test_TimeMatchedAction.cpp
+++ test/test_embedded_TimeMatchedAction/test_TimeMatchedAction.cpp
@@ -25,56 +25,73 @@ private:
 
 void test_match_criteria_year()
 {
-    int x = 0;
-    CIncrement incrementer(x);
+  int x = 0;
+  CIncrement incrementer(x);
 
-    TimeMatchedAction tma;
-    tma.setAction(&incrementer);
-    tma.setMatchCriteria(MatchCriteria::Year);
-    tma.setInvocationDate(DateTime(2000, 2, 15, 12, 30, 30));
-    tma.setComplete(false);
+  TimeMatchedAction tma;
+  tma.setAction(&incrementer);
+  tma.setMatchCriteria(MatchCriteria::Year);
+  tma.setInvocationDate(DateTime(2000, 2, 15, 12, 30, 30));
+  tma.setComplete(false);
 
-    tma.update(DateTime(2000, 2, 15, 12, 30, 29), DateTime(2000, 2, 15, 12, 30, 31));
+  tma.update(DateTime(2000, 2, 15, 12, 30, 29), DateTime(2000, 2, 15, 12, 30, 31));
 
-    TEST_ASSERT_TRUE(x == 1);
-    TEST_ASSERT_TRUE(tma.isComplete());
+  TEST_ASSERT_TRUE(x == 1);
+  TEST_ASSERT_TRUE(tma.isComplete());
 
-    tma.setInvocationDate(DateTime(2000, 2, 15, 13, 45, 45));
-    tma.setComplete(false);
+  tma.setInvocationDate(DateTime(2000, 2, 15, 13, 45, 45));
+  tma.setComplete(false);
 
-    tma.update(DateTime(2000, 2, 15, 13, 46, 45), DateTime(2000, 2, 15, 13, 47, 45));
+  tma.update(DateTime(2000, 2, 15, 13, 46, 45), DateTime(2000, 2, 15, 13, 47, 45));
 
-    TEST_ASSERT_TRUE(x == 1);
-    TEST_ASSERT_FALSE(tma.isComplete());
+  TEST_ASSERT_TRUE(x == 1);
+  TEST_ASSERT_FALSE(tma.isComplete());
 
-    tma.update(DateTime(2000, 2, 15, 13, 43, 45), DateTime(2000, 2, 15, 13, 44, 45));
+  tma.update(DateTime(2000, 2, 15, 13, 43, 45), DateTime(2000, 2, 15, 13, 44, 45));
 
-    TEST_ASSERT_TRUE(x == 1);
-    TEST_ASSERT_FALSE(tma.isComplete());
+  TEST_ASSERT_TRUE(x == 1);
+  TEST_ASSERT_FALSE(tma.isComplete());
 
-    tma.setInvocationDate(DateTime(2000, 2, 15, 13, 45, 45));
+  tma.setInvocationDate(DateTime(2000, 2, 15, 13, 45, 45));
 
-    TEST_ASSERT_TRUE(DateTime(2000, 2, 15, 13, 45, 45) == tma.getInvocationDate());
+  TEST_ASSERT_TRUE(DateTime(2000, 2, 15, 13, 45, 45) == tma.getInvocationDate());
 
-    tma.update(DateTime(2000, 2, 15, 13, 44, 39), DateTime(2000, 2, 15, 13, 46, 10));
+  tma.update(DateTime(2000, 2, 15, 13, 44, 39), DateTime(2000, 2, 15, 13, 46, 10));
 
-    TEST_ASSERT_TRUE(x == 2);
-    TEST_ASSERT_TRUE(tma.isComplete());
+  TEST_ASSERT_TRUE(x == 2);
+  TEST_ASSERT_TRUE(tma.isComplete());
+}
+
+void test_match_criteria_month()
+{
+  int x = 0;
+  CIncrement incrementer(x);
+
+  TimeMatchedAction tma;
+  tma.setAction(&incrementer);
+  tma.setMatchCriteria(MatchCriteria::Month);
+  tma.setInvocationDate(DateTime(2000, 2, 15, 12, 30, 30));
+  tma.setComplete(false);
+
+  tma.update(DateTime(2000, 2, 15, 12, 29, 20), DateTime(2000, 2, 15, 12, 31, 10));
+
+  TEST_ASSERT_EQUAL(1, x);
 }
 
 void setup() {
-    // NOTE!!! Wait for >2 secs
-    // if board doesn't support software reset via Serial.DTR/RTS
-    delay(2000);
+  // NOTE!!! Wait for >2 secs
+  // if board doesn't support software reset via Serial.DTR/RTS
+  delay(2000);
 
-    UNITY_BEGIN();
-    RUN_TEST(test_match_criteria_year);
-    UNITY_END();
+  UNITY_BEGIN();
+  RUN_TEST(test_match_criteria_year);
+  RUN_TEST(test_match_criteria_month);
+  UNITY_END();
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(100);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
 }
