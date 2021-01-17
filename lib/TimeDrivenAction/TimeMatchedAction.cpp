7d7092ba51f9bc9ce7a2a88737fba46bb5ab1b9e
cowdingus <zaki.nadhif45@gmail.com>
2021-01-17 20:27:41 +0700

    Fix bug wrong execution time determination.

--- lib/TimeDrivenAction/TimeMatchedAction.cpp
+++ lib/TimeDrivenAction/TimeMatchedAction.cpp
@@ -99,25 +99,49 @@ namespace tda
       case MatchCriteria::Year:
         return inside(invocationTime, previousTime, currentTime);
       case MatchCriteria::Month:
-        if (!inside(invocationTime.month(), previousTime.month(), currentTime.month()))
-          return false;
+        // To prevent misses if the parameter is only one (only the currentTime)
+        // and currentTime is offset to the future a little bit, so can't use
+        // plain equal operator, it will miss if deltaTime is too big.
+        // Geometric interpretation of timeline made me think of this solution.
+        if (inside(invocationTime.month(), previousTime.month(), currentTime.month()))
+        {
+          // It's obvious if now is one month or plus after the supposed invocation time it should be invoked.
+          if (currentTime.month() > invocationTime.month())
+            return true;
+          // If the month is equal, then fall through to compare smaller units (like day, hour, etc)
+          // Note that currentTime can't be less / lower than invocationTime because that wouldn't pass
+          // the `inside` if statement, that's why I ommited that check.
+        }
       case MatchCriteria::DayOfMonth:
-        if (!inside(invocationTime.day(), previousTime.day(), currentTime.day()))
-          return false;
+        if (inside(invocationTime.day(), previousTime.day(), currentTime.day()))
+        {
+          if (currentTime.day() > invocationTime.day())
+            return true;
+        }
       case MatchCriteria::DayOfWeek:
-        // Skip in case of fall through
+        // Skip in case of fall through, because day of week can be different on each year / month
         if (matchCriteria == MatchCriteria::DayOfWeek)
         {
-          if (!inside(invocationTime.dayOfTheWeek(), previousTime.dayOfTheWeek(), currentTime.dayOfTheWeek()))
-            return false;
+          if (inside(invocationTime.dayOfTheWeek(), previousTime.dayOfTheWeek(), currentTime.dayOfTheWeek()))
+          {
+            if (currentTime.dayOfTheWeek() > invocationTime.dayOfTheWeek())
+              return true;
+          }
         }
       case MatchCriteria::Hour:
-        if (!inside(invocationTime.hour(), previousTime.hour(), currentTime.hour()))
-          return false;
+        if (inside(invocationTime.hour(), previousTime.hour(), currentTime.hour()))
+        {
+          if (currentTime.hour() > invocationTime.hour())
+            return true;
+        }
       case MatchCriteria::Minute:
-        if (!inside(invocationTime.minute(), previousTime.minute(), currentTime.minute()))
-          return false;
+        if (inside(invocationTime.minute(), previousTime.minute(), currentTime.minute()))
+        {
+          if (currentTime.minute() > invocationTime.minute())
+            return true;
+        }
       case MatchCriteria::Second:
+        // There's no need to do any fallthrough checks here, it's already the end.
         return inside(invocationTime.second(), previousTime.second(), currentTime.second());
     }
 
