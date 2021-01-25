7deb71c19ae4f02a553ad5d08b7f78de5ec82288
cowdingus <zaki.nadhif45@gmail.com>
2021-01-25 08:11:04 +0700

    Added repeat functionality to AlarmAction

--- lib/TimeDrivenAction/AlarmAction.hpp
+++ lib/TimeDrivenAction/AlarmAction.hpp
@@ -8,16 +8,27 @@
 
 namespace tda
 {
+    enum class RepeatMode
+    {
+        Once,
+        EveryMinute,
+        EveryHour,
+        EveryDay
+    };
+
     class AlarmAction
     {
     public:
         AlarmAction();
-        AlarmAction(Callback* callback, const DateTime& invocationDate);
+        AlarmAction(Callback* callback, const DateTime& invocationDate, RepeatMode repeatMode = RepeatMode::Once);
 
         void update(const DateTime& currentDateTime);
 
         void setActive(bool active);
         bool isActive() const;
+
+        void setRepeatMode(RepeatMode repeatMode);
+        RepeatMode getRepeatMode() const;
 
         void setAction(Callback* callback);
         Callback* getAction() const;
@@ -29,7 +40,14 @@ namespace tda
         Callback* action = nullptr;
         DateTime invocationTime = DateTime(0, 0, 0, 0, 0, 0);
         bool active = false;
+
+        RepeatMode repeatMode = RepeatMode::Once;
+
+        DateTime getNextInvocationTime() const;
     };
+
+    void setAtMinute(uint8_t minute, const DateTime& now, AlarmAction& alarmAction);
+    void setAtHour(uint8_t hour, const DateTime& now, AlarmAction& alarmAction);
 }
 
 #endif
